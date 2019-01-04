387034ddb15e7ff8e58489e30f818794e98cb7f3
BorislavKostoff <birata@yahoo.com>
2019-01-04 10:18:34 +0200

    !

--- src/com/bk/Main.java
+++ src/com/bk/Main.java
@@ -1,35 +1,16 @@
 package com.bk;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(titleToNumber("AA"));
-        System.out.println(numberToTitle(27));
+        Converter conv = new Converter();
+        System.out.println(conv.titleToNumber("ABC"));
+        System.out.println(conv.numberToTitle(123));
+
     }
 
-    private static int titleToNumber(String s) {
-        if(s == null || s.length() == 0){
-            throw new IllegalArgumentException("Input is not valid!");
-        }
-        int n = s.length();
-        int result = 0;
-        for(int i = n-1; i >= 0; i--){
-            result += Math.pow(26, n-i - 1)*(s.charAt(i) - 64);
-        }
-        return result;
-    }
-
-    private static String numberToTitle(int n){
-        if (n == 0) {
-            throw  new IllegalArgumentException("Input is not valid!");
-        }
-        String s = "";
-        while(n > 0)
-        {
-            n--;
-            s = String.valueOf((char)(n%26 + 'A')) + s;
-            n /= 26;
-        }
-        return s;
-    }
 }
