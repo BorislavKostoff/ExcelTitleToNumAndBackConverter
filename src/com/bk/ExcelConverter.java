617d042d9d7691ff320257c77ef522c9f44d945c
BorislavKostoff <birata@yahoo.com>
2019-01-04 15:02:42 +0200

    !

--- src/com/bk/ExcelConverter.java
+++ src/com/bk/ExcelConverter.java
@@ -3,8 +3,15 @@ package com.bk;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 
-public class ExcelConverter {
+public class ExcelConverter extends JFrame {
+
+
+    Scanner sc = new Scanner(System.in);
+
+    Converter conv = new Converter();
+
     private JPanel panel1;
     private JLabel labelName;
     private JTextField text_columnName;
@@ -18,9 +25,18 @@ public class ExcelConverter {
 
 
     public ExcelConverter() {
+
+        setLocationRelativeTo(this);
+        setSize(520,250);
+        setContentPane(panel1);
+
+
+
+
         convertButtonColumnName.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                result_text_result.setText(String.valueOf(conv.titleToNumber("ABC")));
 
 
             }
@@ -28,9 +44,11 @@ public class ExcelConverter {
         convertButtonNumberColumn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                result_text_result.setText(conv.numberToTitle(123));
 
             }
         });
+
 
     }
 }
--- src/com/bk/Main.java
+++ src/com/bk/Main.java
@@ -1,10 +1,14 @@
 package com.bk;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        new ExcelConverter().setVisible(true);
+
         Scanner sc = new Scanner(System.in);
 
         Converter conv = new Converter();
